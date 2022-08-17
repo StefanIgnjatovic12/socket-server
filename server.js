8ad15f1cf08553d8f708a6a770f4971954241db3
Stefan <sper1234@gmail.com>
2022-08-17 18:18:31 +0200

    Fix cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const server = require("http").createServer();
 const io = require("socket.io")(server, {
     cors: {
-        origin: "*",
+        origin: "https://drf-react-chat-frontend.herokuapp.com",
     },
 });
 
